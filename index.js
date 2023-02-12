5ebc04ef43acf4dcbcc18078c213fcbae09990e9
asaad-tt <aasadshumon86@gmail.comm>
2023-02-12 22:57:32 +0600

    dashboard complete and finalize

--- index.js
+++ index.js
@@ -2,14 +2,72 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-// middlewares
-app.use(cors());
+// Middlewares
+const whitelist = ["http://localhost:3000", "https://aircnc-a740e.web.app"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// Decode JWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log(decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// Send Email
+const sendMail = (emailData, email) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: emailData?.subject,
+    html: `<p>${emailData?.message}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
 
 // Database Connection
 const uri = process.env.DB_URI;
@@ -25,11 +83,24 @@ async function run() {
     const usersCollection = client.db("luxuryNestDb").collection("users");
     const bookingsCollection = client.db("luxuryNestDb").collection("bookings");
 
+    // Verify Admin
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      console.log("Admin true");
+      next();
+    };
+
     // Save user email & generate JWT
-    // --- save user data to database and generate a token and send it to frontend & save local storage--
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
+
       const filter = { email: email };
       const options = { upsert: true };
       const updateDoc = {
@@ -40,58 +111,54 @@ async function run() {
         updateDoc,
         options
       );
-      console.log(result);
 
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1d",
       });
-      console.log(token);
+      console.log(result);
       res.send({ result, token });
     });
 
-    // Save a booking
-    app.post("/bookings", async (req, res) => {
-      const bookingData = req.body;
-      const result = await bookingsCollection.insertOne(bookingData);
-      console.log(result);
-      res.send(result);
-    });
-
-    // Get All Bookings
-    app.get("/bookings", async (req, res) => {
-      let query = {};
-      const email = req.query.email;
-      if (email) {
-        query = {
-          guestEmail: email,
-        };
-      }
-
-      const booking = await bookingsCollection.find(query).toArray();
-      console.log(booking);
-      res.send(booking);
-    });
-
-    // Get a single user by email
-    app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-
-      const user = await usersCollection.findOne(query);
-      console.log(user.role);
-      res.send(user);
-    });
-
-    // Get all users
-    app.get("/users", async (req, res) => {
-      const users = await usersCollection.find().toArray();
-      console.log(users);
+    // Get All User
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
       res.send(users);
+    });
+
+    // Get A Single User
+    app.get("/user/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
     });
 
     // Get All Homes
     app.get("/homes", async (req, res) => {
       const query = {};
+      const cursor = homesCollection.find(query);
+      const homes = await cursor.toArray();
+      res.send(homes);
+    });
+
+    // Get All Homes for host
+    app.get("/homes/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = {
+        "host.email": email,
+      };
       const cursor = homesCollection.find(query);
       const homes = await cursor.toArray();
       res.send(homes);
@@ -105,11 +172,114 @@ async function run() {
       res.send(home);
     });
 
+    // Delete a home
+    app.delete("/home/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await homesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Update A Home
+    app.put("/homes", verifyJWT, async (req, res) => {
+      const home = req.body;
+      console.log(home);
+
+      const filter = {};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: home,
+      };
+      const result = await homesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
     // Post A Home
-    app.post("/homes", async (req, res) => {
+    app.post("/homes", verifyJWT, async (req, res) => {
       const home = req.body;
       console.log(home);
       const result = await homesCollection.insertOne(home);
+      res.send(result);
+    });
+
+    // Get search result
+    app.get("/search-result", async (req, res) => {
+      const query = {};
+      const location = req.query.location;
+      if (location) query.location = location;
+
+      console.log(query);
+      const cursor = homesCollection.find(query);
+      const homes = await cursor.toArray();
+      res.send(homes);
+    });
+
+    // Get Bookings
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = {
+          guestEmail: email,
+        };
+      }
+      const cursor = bookingsCollection.find(query);
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+
+    // Get a single booking
+    app.get("/booking/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
+    });
+
+    // Save bookings
+    app.post("/bookings", verifyJWT, async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingsCollection.insertOne(booking);
+
+      console.log("result----->", result);
+      sendMail(
+        {
+          subject: "Booking Successful!",
+          message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+        },
+        booking?.guestEmail
+      );
+      res.send(result);
+    });
+
+    // Create Payment Intent
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const price = req.body.price;
+      console.log(price);
+      const amount = parseFloat(price) * 100;
+
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        res.send({ clientSecret: paymentIntent.client_secret });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // Cancel a booking
+    app.delete("/booking/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -121,9 +291,9 @@ async function run() {
 run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Server is running ... in session");
+  res.send("Server is running Luxury Server... in session");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running...on Pandora moon ${port}`);
+  console.log(`Server is running Luxury Nest...on ${port}`);
 });
